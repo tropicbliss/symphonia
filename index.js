ddc68934bf3c4e43a96e043e7ba9bfdf0e095958
tropicbliss <tropicbliss@protonmail.com>
2022-12-09 10:51:44 +0800

    E

--- index.js
+++ index.js
@@ -25,24 +25,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.android-arm64.node')
+            nativeBinding = require('./package-template.android-arm64.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-android-arm64')
+            nativeBinding = require('@napi-rs/package-template-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.android-arm-eabi.node')
+            nativeBinding = require('./package-template.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-android-arm-eabi')
+            nativeBinding = require('@napi-rs/package-template-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -55,36 +55,36 @@ switch (platform) {
   case 'win32':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.win32-x64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.win32-x64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.win32-x64-msvc.node')
+            nativeBinding = require('./package-template.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-win32-x64-msvc')
+            nativeBinding = require('@napi-rs/package-template-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'ia32':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.win32-ia32-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.win32-ia32-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.win32-ia32-msvc.node')
+            nativeBinding = require('./package-template.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-win32-ia32-msvc')
+            nativeBinding = require('@napi-rs/package-template-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.win32-arm64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.win32-arm64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.win32-arm64-msvc.node')
+            nativeBinding = require('./package-template.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-win32-arm64-msvc')
+            nativeBinding = require('@napi-rs/package-template-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -97,24 +97,24 @@ switch (platform) {
   case 'darwin':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.darwin-x64.node')
+            nativeBinding = require('./package-template.darwin-x64.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-darwin-x64')
+            nativeBinding = require('@napi-rs/package-template-darwin-x64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.darwin-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.darwin-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.darwin-arm64.node')
+            nativeBinding = require('./package-template.darwin-arm64.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-darwin-arm64')
+            nativeBinding = require('@napi-rs/package-template-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -128,12 +128,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'symphonia.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'package-template.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./symphonia.freebsd-x64.node')
+        nativeBinding = require('./package-template.freebsd-x64.node')
       } else {
-        nativeBinding = require('@tropicbliss/symphonia-freebsd-x64')
+        nativeBinding = require('@napi-rs/package-template-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -143,23 +143,23 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, 'symphonia.linux-x64-musl.node'))
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-x64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./symphonia.linux-x64-musl.node')
+              nativeBinding = require('./package-template.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@tropicbliss/symphonia-linux-x64-musl')
+              nativeBinding = require('@napi-rs/package-template-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, 'symphonia.linux-x64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-x64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./symphonia.linux-x64-gnu.node')
+              nativeBinding = require('./package-template.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@tropicbliss/symphonia-linux-x64-gnu')
+              nativeBinding = require('@napi-rs/package-template-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -168,23 +168,23 @@ switch (platform) {
         break
       case 'arm64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, 'symphonia.linux-arm64-musl.node'))
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./symphonia.linux-arm64-musl.node')
+              nativeBinding = require('./package-template.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@tropicbliss/symphonia-linux-arm64-musl')
+              nativeBinding = require('@napi-rs/package-template-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, 'symphonia.linux-arm64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./symphonia.linux-arm64-gnu.node')
+              nativeBinding = require('./package-template.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@tropicbliss/symphonia-linux-arm64-gnu')
+              nativeBinding = require('@napi-rs/package-template-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -192,12 +192,12 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'symphonia.linux-arm-gnueabihf.node'))
+        localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm-gnueabihf.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./symphonia.linux-arm-gnueabihf.node')
+            nativeBinding = require('./package-template.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('@tropicbliss/symphonia-linux-arm-gnueabihf')
+            nativeBinding = require('@napi-rs/package-template-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
@@ -218,6 +218,6 @@ if (!nativeBinding) {
   throw new Error(`Failed to load native binding`)
 }
 
-const { play } = nativeBinding
+const { plus100 } = nativeBinding
 
-module.exports.play = play
+module.exports.plus100 = plus100
