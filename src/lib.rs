6429b9f5f062ed818f8a2eb788b96612626227d9
tropicbliss <tropicbliss@protonmail.com>
2022-12-09 13:59:17 +0800

    Clean up code

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use napi::{
   bindgen_prelude::{Buffer, Error, Status},
   Result,
 };
-use rodio::{source::SineWave, Decoder, OutputStream, Sink, Source};
+use rodio::{source::SineWave, Decoder, OutputStream, Sample, Sink, Source};
 use std::{io::Cursor, time::Duration};
 
 #[macro_use]
@@ -20,30 +20,27 @@ pub struct Options {
 /// The speed and volume is both set to 1.0 by default.
 /// Note that calling this function blocks the main thread so use worker threads to make it non-blocking.
 pub fn play_from_buf(buf: Buffer, opt: Option<Options>) -> Result<()> {
-  let (_stream, stream_handle) =
-    OutputStream::try_default().map_err(|e| Error::new(Status::GenericFailure, e.to_string()))?;
-  let sink =
-    Sink::try_new(&stream_handle).map_err(|e| Error::new(Status::GenericFailure, e.to_string()))?;
-  if let Some(options) = opt {
-    if let Some(volume) = options.volume {
-      sink.set_volume(volume as f32);
-    }
-    if let Some(speed) = options.speed {
-      sink.set_speed(speed as f32);
-    }
-  }
   let buffer: Vec<u8> = buf.into();
   let cursor = Cursor::new(buffer);
   let source =
     Decoder::new(cursor).map_err(|e| Error::new(Status::GenericFailure, e.to_string()))?;
-  sink.append(source);
-  sink.sleep_until_end();
+  play(source, opt)?;
   Ok(())
 }
 
 #[napi]
 /// Note that calling this function blocks the main thread so use worker threads to make it non-blocking.
 pub fn play_from_sine(freq: u32, ms: f64, opt: Option<Options>) -> Result<()> {
+  let source = SineWave::new(freq as f32).take_duration(Duration::from_millis(ms as u64));
+  play(source, opt)?;
+  Ok(())
+}
+
+fn play<S>(source: S, opt: Option<Options>) -> Result<()>
+where
+  S: Source + Send + 'static,
+  S::Item: Sample + Send,
+{
   let (_stream, stream_handle) =
     OutputStream::try_default().map_err(|e| Error::new(Status::GenericFailure, e.to_string()))?;
   let sink =
@@ -56,7 +53,6 @@ pub fn play_from_sine(freq: u32, ms: f64, opt: Option<Options>) -> Result<()> {
       sink.set_speed(speed as f32);
     }
   }
-  let source = SineWave::new(freq as f32).take_duration(Duration::from_millis(ms as u64));
   sink.append(source);
   sink.sleep_until_end();
   Ok(())
